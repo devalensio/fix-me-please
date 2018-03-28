6a42373f1f016d4ce89acd0c57a4b7791bbe83db
Devdots <devdots@Devdotss-MacBook-Pro.local>
2018-03-28 09:32:50 +0700

    initial commit

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
-const app = express;
+const mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
+mongoose.connect('mongodb://localhost:27017/api-crud-mongoose', (err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
 
-var books = require('routes/books');
+var books = require('./routes/books');
 var transactions = require('routes/transactions');
+
+const app = express();
 
 app.use('/books', books);
 app.use('/transactions', transactions);
